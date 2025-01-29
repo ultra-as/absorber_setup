08e0568c5acbbbccee93cd7a1262c6819671692f
ultra-as <q15ashah@wgsf.net>
2025-01-29 13:00:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ setBackgroundColour("black")
 
 
 class Creature:
-    def __init__(self,x,y,image,  size):
+    def __init__(self,x,y,image,  size, ranking, speed, powerUp):
         self.x = x
         self.y = y
         self.size = size   # size is a percentage of the full size image
+        self.ranking = ranking
+        self.speed = speed
+        self.powerUp = powerUp
+        
+        
         self.sprite = makeSprite(image)
         moveSprite(self.sprite,self.x,self.y,centre=True)
         transformSprite(self.sprite, 0, self.size/100)
         showSprite(self.sprite)
 
 
+class Player(Creature):
+    def __init__(self,x,y,image,size,ranking,speed,powerUp):
+        super().__init__(x,y,image,size, ranking, speed,powerUp)
+
+
 setAutoUpdate(False)
-c1 = Creature(random.randint(0,1000),random.randint(0,1000), "enemy.png", random.randint(10,100)) 
+c1 = Creature(random.randint(0,1000),random.randint(0,1000), "enemy1.png", random.randint(10,100),0,0,0)
+p1 = Player(random.randint(0,1000), random.randint(0,1000), "player1.png", random.randint(10,100),0,0,0)
 
 updateDisplay()
         
 endWait()
+
