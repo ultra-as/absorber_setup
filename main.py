8cc6bcefdfb687e4aa841969e89fbc4c61d65502
ultra-as <q15ashah@wgsf.net>
2025-02-07 12:56:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Creature:
         self.y = y
         self.size = size   # size is a percentage of the full size image
         self.ranking = ranking
-        self.speed = random.randint(5,15)
+        self.speed = random.randint(1,15)
         self.angle = random.randint(1,360)
         self.powerUp = powerUp
         
@@ -24,55 +24,88 @@ class Creature:
         transformSprite(self.sprite, self.angle, self.size/100)
         showSprite(self.sprite)
     
-    def move(self):
-        xSpeed = self.speed * math.cos(self.angle/180*math.pi)
-        ySpeed = self.speed * math.sin(self.angle/180*math.pi)
-        self.x = (self.x + xSpeed) % width
-        self.y = (self.y + ySpeed) % height
-        moveSprite(self.sprite,self.x,self.y, centre = True)
+    def move(self, player):
+        if(not player.end):
+            xSpeed = self.speed * math.cos(self.angle/180*math.pi)
+            ySpeed = self.speed * math.sin(self.angle/180*math.pi)
+            self.x = (self.x + xSpeed) % (width*10)
+            self.y = (self.y + ySpeed) % (height*10)
+            #self.x = (self.x - player.xPos) % (width*10)
+            #self.y = (self.x - player.yPos) % (height*10)
+            
+            moveSprite(self.sprite,(500+(self.x - player.x)),(500+(self.y - player.y)), centre = True)
+        
+        
 
 
 class Player(Creature):
     def __init__(self,x,y,image,size,ranking,powerUp):
         super().__init__(x,y,image,size, ranking, powerUp)
+        self.lastTrail = clock()
+        moveSprite(self.sprite,500,500,centre=True)
+        self.end = False
+
         
     
     def move(self, creatures):
-        xPos = mouseX() - self.x
-        yPos = mouseY() - self.y
-        distance = math.sqrt(xPos**2 + yPos**2)
-        self.speed = distance / 200 * 10
-        self.angle = math.degrees(math.atan2(yPos,xPos))
-        transformSprite(self.sprite,self.angle,self.size/100)
-        super().move()
+        if(not self.end):
+            now = clock()
+            if(now > self.lastTrail + 1000):
+                self.lastTrail = clock()
+                
+            
+            xPos = mouseX() - 500
+            yPos = mouseY() - 500
+            distance = math.sqrt(xPos**2 + yPos**2)
+            self.speed = distance / 200 * 10
+            self.angle = math.degrees(math.atan2(yPos,xPos))
+            
+            transformSprite(self.sprite,self.angle,self.size/100)
+            
+            xSpeed = self.speed * math.cos(self.angle/180*math.pi)
+            ySpeed = self.speed * math.sin(self.angle/180*math.pi)
+            self.x = (self.x + xSpeed) % (width*10)
+            self.y = (self.y + ySpeed) % (height*10)
+            
+         
+
+            
+            for c in creatures:
+                if(touching(self.sprite,c.sprite)):
+                    if(self.size > c.size):
+                        print("Nom")
+                        creatures.remove(c)
+                        hideSprite(c.sprite)
+                        self.size += c.speed*0.2
+                    elif(self.size < c.size):
+                        self.end = True
+    
+    def trail(self):
         
-        for c in creatures:
-            if(touching(self.sprite,c.sprite)):
-                if(self.size > c.size):
-                    print("Nom")
-                    creatures.remove(c)
-                    hideSprite(c.sprite)
-                    self.size += 5
-                elif(self.size < c.size):
-                    print("Dead")
     
     
+
+def drawBorder(player):
+    clearShapes()
+    drawRect(500-player.x, 500-player.y, 10000,10000, (20,20,20),0)
+    drawRect(500-player.x, 500-player.y, 10000,10000, (255,255,255),5)
 
 
 setAutoUpdate(False)
 creatures = []
 
-for i in range(5):
-    creatures.append(Creature(random.randint(0,1000),random.randint(0,1000), "enemy1.png", random.randint(10,100),0,0))
+for i in range(200):
+    creatures.append(Creature(random.randint(0,10000),random.randint(0,10000), "enemy1.png", random.randint(15,150),0,0))
 
-p1 = Player(random.randint(0,1000), random.randint(0,1000), "player1.png", random.randint(10,100),0,0)
+p1 = Player(5000, 5000, "player1.png", random.randint(30,40),0,0)
 
-updateDisplay()
 
-while(True):
+
+while(not p1.end):
     for c in creatures:
-        c.move()
+        c.move(p1)
     p1.move(creatures)
+    drawBorder(p1)
     updateDisplay()
     tick(50)
         
